32603fc152d169e11ed3b2f79493fd7134c681b2
hossein <amamyrag@gmail.com>
2026-07-17 11:36:02 +0330

    show

--- hossein.php
+++ hossein.php
@@ -9,7 +9,7 @@ try{
     $result=$pdo->prepare("INSERT INTO users (name , username) values (:name , :username)");
     $result->execute(["name"=>"hossein",
     "username"=>"hossein23"]);
-
+    $show=$result->fetch();
 }catch(Exception $e){
     echo $e->getMessage();
 }
