485a111a4abce291ad22bd2c7d6ecdcc26760edc
hossein <amamyrag@gmail.com>
2026-07-17 11:31:44 +0330

    pdo php

--- hossein.php
+++ hossein.php
@@ -3,3 +3,13 @@ $name= "hossein";
 if($name="hossein"){
     echo "hello hossein";
 }
+try{
+    $pdo=new PDO("mysql:host=localhost;dbname=roocket" , "root");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $result=$pdo->prepare("INSERT INTO users (name , username) values (:name , :username)");
+    $result->execute(["name"=>"hossein",
+    "username"=>"hossein23"]);
+
+}catch(Exception $e){
+    echo $e->getMessage();
+}
